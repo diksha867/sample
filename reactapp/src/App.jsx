83ec4027f5f83daeba67165378e5a7368b4dd9bc
Diksha Sharma <dikshash867@gmail.com>
2026-04-30 15:16:16 +0530

    profile

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -4,6 +4,8 @@ import TopBar from './component/TopBar'
 import Article from './component/Article'
 import HomePage from './component/HomePage'
 import Contact from './component/contact' 
+import { Routes, Route } from 'react-router-dom'
+import Profile from './component/profile'
 
 function App() {
 
@@ -12,9 +14,12 @@ function App() {
 
   return (
     <>
-      <HomePage />
-      <TopBar />
-      <Article title={title} />
+      <Routes>
+        <Route path="/" element={<><HomePage /><TopBar /></>} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/article" element={<Article  title={title}  />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
     </>
   )
 }
--- reactapp/src/component/TopBar.jsx
+++ reactapp/src/component/TopBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link ,Navlink} from 'react-router-dom'
 
 function TopBar() {
   return (
     <div>
       <h1>Logo</h1>
       <ul>
-        <li>Entertainment</li>
-        <li>Sports</li>
-        <li>Politics</li>
+        <link to="/"><li>HomePage</li></link>
+        <Navlink style={({isActive})=>({color:isActive?"red":"black"})} to="/"><li>HomePage</li></Navlink>
+        <Navlink style={({isActive})=>({color:isActive?"red":"black"})} to="/article"><li>Article</li></Navlink>
+        <link to="/contact"><li>Contact</li></link>
+        <link to="/Article"><li>Article</li></link>
       </ul>
     </div>
   )
